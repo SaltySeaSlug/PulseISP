7ea685203cb3593e935c0c80fbc7fd907ffab463
SaltySeaSlug <SaltySeaSlug@users.noreply.github.com>
2021-01-12 15:43:05 +0200

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	.idea/workspace.xml
    #	install.sh

--- site/install/index.php
+++ site/install/index.php
@@ -69,7 +69,7 @@ if($_POST) {
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>FlexRadius - Installation Wizard</title>
+    <title>PulseISP - Installation Wizard</title>
 
     <!-- CSS -->
     <link rel="stylesheet" href="../assets/plugins/bootstrap/css/bootstrap.css">
